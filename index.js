84931f33e2ae4117b7c59844ceeea47687869259
= <keremakcalioglu@hotmail.com>
2022-04-08 22:00:08 +0300

    created basic functionality and style

---
+++ index.js
@@ -0,0 +1,50 @@
+const GRID_MEASURE = 512; 
+
+let eraseMod = false;
+
+const grid = document.querySelector(".grid");
+const colorPicker = document.querySelector("#color-picker")
+const gridSizeSlider = document.querySelector("#size-slider")
+const eraserToggle = document.querySelector("#eraser-toggle")
+const clearButton = document.querySelector("#clear-button")
+const rainbowToggle = document.querySelector("#rainbow-mode-toggle")
+
+colorPicker.addEventListener("change", (e) => color = e.target.value)
+gridSizeSlider.addEventListener("change",  createGrid)
+eraserToggle.addEventListener("click", () => {eraseMod = !eraseMod; eraserToggle.classList.toggle("option-items-active")} )
+clearButton.addEventListener("click", createGrid)
+
+
+color = colorPicker.value;
+createGrid()
+
+function createGrid() {
+  const gridSize = gridSizeSlider.value;
+  const oldCells = document.querySelectorAll(".cell");
+  oldCells.forEach((e) => {
+    e.remove()
+  })
+  for (let i = 0; i < gridSize**2; i++) {
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+    cell.addEventListener("mouseover", handleCell);
+    cell.addEventListener("mousedown", handleCell);
+    cell.style.cssText = `width: ${GRID_MEASURE / gridSize}px; height: ${GRID_MEASURE / gridSize}px;`;
+    grid.appendChild(cell)
+  }
+}
+
+function handleCell(e) {
+  console.log(color)
+  if (!eraseMod) {
+    if (e.buttons == 1 || e.buttons == 3) {
+      e.currentTarget.style.backgroundColor = color
+    } 
+  } else {
+    e.currentTarget.style.backgroundColor = "#DBDBDB"
+  }
+  
+}
+
+
+
